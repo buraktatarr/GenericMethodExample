287aff32c6d3918ef8c89271adb757cf97f514ca
Burak Tatar <mburaktatar@gmail.com>
2025-04-13 21:15:26 +0300

    Inıtial Commit.

--- src/GenericMethodExample.java
+++ src/GenericMethodExample.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class GenericMethodExample {
+
+        // Generic printArray metodumuzu yazıyoruz;
+
+        public static <T> void printArray(T[] array) {
+            for (T eleman : array) {
+                System.out.print(eleman + " ");
+            }
+            System.out.println(); // Yeni satıra geç (boşluk bırak)
+        }
+
+        public static void main(String[] args) {
+            // Integer dizisi;
+            Integer[] intArray = {1, 2, 3, 4, 5};
+
+            // String dizisi;
+            String[] strArray = {"Java", "Generics", "Ödev"};
+
+            // Generic methodu çağırma;
+            printArray(intArray);
+            printArray(strArray);
+        }
 }
